fef5c62ca40a60462628f462e842f08756590dc7
Seardagon <jjs7965@gmail.com>
2021-10-09 17:43:20 -0400

    First revision, added APM executables, and barebones script to start them

--- apm.sh
+++ apm.sh
@@ -3,4 +3,9 @@
 # NSSA 220 Project 1: APM Tool
 # Sean O'Beirne, Mani Perez, Joshua Sylvester
 
-#test
+./project1_executables/APM1 192.168.28.2
+./project1_executables/APM2 192.168.28.2 
+./project1_executables/APM3 192.168.28.2
+./project1_executables/APM4 192.168.28.2
+./project1_executables/APM5 192.168.28.2
+./project1_executables/APM6 192.168.28.2
