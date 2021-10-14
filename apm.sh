1257d5f2cc8c78ebdd1d69f262a363070ae07faa
Seardagon <jjs7965@rit.edu>
2021-10-13 21:35:40 -0400

    Added RX/TX data collection to apm.sh

--- apm.sh
+++ apm.sh
@@ -4,12 +4,18 @@
 # Sean O'Beirne, Mani Perez, Joshua Sylvester
 
 cpu_and_mem_stats() {
-cpu1=$(ps aux | grep '/APM$1' | cut -f 7 -d " " | tail -1);
-mem1=$(ps aux | grep '/APM$1' | cut -f 9 -d " " | tail -1);
-echo "CPU Usage for APM$1: $cpu1%"
-echo "Memory Usage for APM$1: $mem1%"
+cpu1=$(ps aux | grep "/APM$1" | awk '{print $3;exit;}');
+mem1=$(ps aux | grep "/APM$1" | awk '{print $4;exit;}');
+echo "CPU Usage for APM$1: $cpu1"
+echo "Memory Usage for APM$1: $mem1"
 }
 
+rx_and_tx() {
+rx=$(ifstat | grep "ens33" | awk '{print $6;exit;}');
+tx=$(ifstat | grep "ens33" | awk '{print $8;exit;}');
+echo "RX Data rate: ${rx}B/S" 
+echo "TX Data rate: ${tx}B/S"
+}
 ./project1_executables/APM1 192.168.28.2 &
 ./project1_executables/APM2 192.168.28.2 &
 ./project1_executables/APM3 192.168.28.2 &
@@ -17,18 +23,29 @@ echo "Memory Usage for APM$1: $mem1%"
 ./project1_executables/APM5 192.168.28.2 &
 ./project1_executables/APM6 192.168.28.2 &
 
-i=12
 j=1
-while [ $i -gt 0 ];
+sec=1
+sleep 1
+while [ $sec -lt 21 ];
 do
-while [ $j -lt 6 ];
+if ! (( sec % 5 ));
+then
+echo "---------------------"
+echo "$sec seconds"
+while [ $j -lt 6 ]
 do
 cpu_and_mem_stats $j
-j=$((j + 1));
-done 
+j=$(($j + 1));
+done
+echo "---------------------"
+fi
+if [ $sec -gt 4 ]
+then
+rx_and_tx
+fi
 j=1
-sleep 5
-i=$((i - 1));
+sleep 1
+sec=$((sec + 1));
 done
 
 killall -9 APM1
