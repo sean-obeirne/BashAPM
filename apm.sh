09d934abd0722e1a3f6a227ecc6b3414a658d763
Sean O'Beirne <sean.t.obeirne@gmail.com>
2021-10-19 14:41:37 -0400

    general code (documentation) cleanup

--- apm.sh
+++ apm.sh
@@ -3,6 +3,7 @@
 # NSSA 220 Project 1: APM Tool
 # Sean O'Beirne, Mani Perez, Joshua Sylvester
 
+# Spawn all necessary APM proccesses and create csv files
 spawn_processes(){
    # grab NIC ip
    local ip=$(ifconfig ens33 | grep "inet" | head -1 | cut -f 10 -d " ")
@@ -10,35 +11,41 @@ spawn_processes(){
    # cleanup old metrics files
    rm -f APM?_metrics.csv system_metrics.csv
 
-   # spawn procs & create files
+   # spawn procs & create files with headers
    for (( i = 1; i <= $PROCS; i++ ))
    {
       ./project1_executables/APM$i $ip &
       echo "seconds,%CPU,%memory" > 'APM'$i'_metrics.csv'
    }
    echo "seconds,RX data rate,TX data rate,disk writes,available disk capacity" > system_metrics.csv
+
+   # begin ifstat
    ifstat -a -d 1 ens33
 }
 
+# Trap function
+# Kill all spawned processes and ifstat, discontinue running loop
 cleanup(){
    killall -r "APM[1-6]"
    killall "ifstat"
+   cont=0
 }
 
-PROCS=6
-f1="APM1_metrics.csv"
+PROCS=6  # constant number of APM processes to spawn
 
 trap cleanup EXIT
-
 spawn_processes
-f=1
-sec=1
+
+cont=1   # we execute infinitely until cont is not 1
+sec=1    # running number of seconds we have been running
 sleep 1
-while [ $f -eq 1 ];
+
+# main execution loop
+while [ $cont -eq 1 ];
 do
-   if ! (( sec % 5 ));
+   if !(( sec % 5 ));
    then
-      echo $sec
+      echo "running execution time: $sec seconds"
 
       #process-level metrics
       for (( i = 1; i <= $PROCS; i++ )){
@@ -52,6 +59,7 @@ do
       iostat | grep "sda" | awk '{printf $4 ","}' >>  system_metrics.csv
       df -hm / | grep "root" | awk '{printf $4 "\n"}' >> system_metrics.csv
    fi
+
    sleep 1
    sec=$((sec + 1));
 done
